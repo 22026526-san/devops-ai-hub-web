927836a0cac06370ab5a4d61ff545eaa8cdfb0f8
Quang Sang <sangv6548@gmail.com>
2026-04-22 09:31:59 +0700

    feat : add enum

---
+++ src/common/enums/index.js
@@ -0,0 +1,21 @@
+export const POST_STATUS = {
+  PUBLISHED: 'Published',
+  DELETED: 'Deleted',
+};
+
+export const POST_VISIBILITY = {
+  PUBLIC: 'Public',
+  FOLLOWERS: 'Followers',
+  PRIVATE: 'Private',
+};
+
+export const POST_TYPES = {
+  QUESTION: 'Question',
+  PIPELINE: 'Pipeline',
+};
+
+export const USER_ROLES = {
+  ADMIN: 'Admin',
+  USER: 'User',
+  GUEST: 'Guest',
+};
